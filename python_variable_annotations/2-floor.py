95130480ed305be457dd8353ebff764e9cd411a9
Alexpc18 <alexanderpc18@hotmail.com>
2024-02-18 20:05:52 -0800

    feat script 2 and 3

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+This function takes a float as input and
+returns its closest minor integer value.
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Return the floor of a float.
+
+    Args:
+        n (float): The float to find the floor of.
+
+    Returns:
+        int: The floor value of the float.
+    """
+    return math.floor(n)
---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+We define a function called 'to_str' that takes a floating number 'n'
+as input and returns its string representation using the 'str()' function.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Convert a float to its string representation.
+
+    Parameters:
+        n (float): The float to be converted to a string.
+
+    Returns:
+        str: The string representation of the float.
+    """
+    return str(n)
