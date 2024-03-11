ab0997190d887d9de4062cc5b7493013087faa65
Alexpc18 <alexanderpc18@hotmail.com>
2024-03-11 04:14:06 +0000

    redoing script 2

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -1,32 +1,19 @@
 #!/usr/bin/env python3
-"""Pagination of Popular_Baby_Name.csv"""
-
+""" Hypermedia pagination """
 import csv
-import math
-from typing import List
-
-
-index_range = __import__('0-simple_helper_function').index_range
-
-
-def verify_enter_positive(num) -> None:
-    """
-    Define a function to verify that a number is a positive integer
-    """
-    assert isinstance(num, int) and num > 0, AssertionError
+from math import ceil
+from typing import List, Tuple, Dict
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Cached dataset"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -36,21 +23,46 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """
-        Returns a page of the database.
+        """Get the page"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
-        Args:
-            page (int): The page number to retrieve (default is 1).
-            page_size (int): The size of the page (default is 10).
+        range: Tuple = index_range(page, page_size)
+        pagination: List = self.dataset()
 
-        Returns:
-            List[List]: The list of records corresponding to the
-                        requested page.
-        """
-        # Verify that page and page_size are positive integers
-        verify_enter_positive(page)
-        verify_enter_positive(page_size)
+        return (pagination[range[0]:range[1]])
 
-        start_index, end_index = index_range(page, page_size)
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """Range of the page"""
 
-        return self.dataset()[start_index: end_index]
+        data = []
+        try:
+            data = self.get_page(page, page_size)
+        except AssertionError:
+            return {}
+
+        dataset: List = self.dataset()
+        totalpag: int = len(dataset) if dataset else 0
+        totalpag = ceil(totalpag / page_size)
+        prevpag: int = (page - 1) if (page - 1) >= 1 else None
+        nextpag: int = (page + 1) if (page + 1) <= totalpag else None
+
+        hypermedia: Dict = {
+            'page_size': page_size,
+            'page': page,
+            'data': data,
+            'next_page': nextpag,
+            'prev_page': prevpag,
+            'total_pages': totalpag,
+        }
+
+        return hypermedia
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Range of the page"""
+
+    final_size: int = page * page_size
+    start_size: int = final_size - page_size
+
+    return (start_size, final_size)
