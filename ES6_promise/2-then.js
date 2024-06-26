0247c54198042274cd073d5a086a3d468e619049
Alexpc18 <alexanderpc18@hotmail.com>
2024-06-26 06:11:06 +0000

    feat script 0 to 3

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,9 @@
+// This script adds three handlers to the function
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
+}
---
+++ ES6_promise/README.md
@@ -0,0 +1,1 @@
+ES6 Promises in JavaScript streamline asynchronous programming by representing future results that can be resolved or rejected. They use then() for handling successful outcomes and catch() for errors, offering a structured approach to manage asynchronous tasks effectively.
